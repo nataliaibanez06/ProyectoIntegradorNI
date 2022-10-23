748902fe595420761e031cc9d73aff1f74415667
nataliaibanez06 <natii.ibanez2@gmail.com>
2022-10-23 00:39:14 -0300

    backend

---
+++ ProyectoIntegradorNI/BackEnd/src/main/java/com/portfolio/nvi/NviApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.nvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NviApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegradorNI/BackEnd/src/test/java/com/portfolio/nvi/NviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.nvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
