30f9c029581dc3aa934e0cb4d9ec0d2ee6b657b0
nataliaibanez06 <natii.ibanez2@gmail.com>
2022-10-22 21:59:55 -0300

    creacion del footer y proyecto

--- ProyectoIntegradorNI/src/app/app.module.ts
+++ ProyectoIntegradorNI/src/app/app.module.ts
@@ -5,24 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { FormsModule } from '@angular/forms';
 import { HysComponent } from './components/hys/hys.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    SocialComponent,
     BannerComponent,
     AcercaDeComponent,
     EducacionComponent,
-    HysComponent
+    HysComponent,
+    FooterComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
